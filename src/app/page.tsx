5b0ecec9a78a7f170739222614c02a9c50a57a50
Ernest Nnamdi <ernestnnamdi@Ernests-MacBook-Pro-2.local>
2024-08-08 17:49:39 +0100

    chore: fixed ui, adding shadcn components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,19 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <main className="flex min-h-screen flex-col items-center justify-center p-24">
-        <h1 className="text-4xl font-bold mb-8">Morph Faucet</h1>
-        <FaucetForm />
-      </main>
+    <main className="flex flex-col h-screen items-center justify-center">
+      <section className="py-12 flex flex-col items-center text-center gap-8">
+        <h1 className="text-4xl font-bold">Morph Holesky Faucet</h1>
+        <p className="text-2xl text-muted-foreground">
+          Claim testnet ETH to your wallet. You can only claim 0.1ETH per day.
+        </p>
+      </section>
+
+      <section className="flex flex-col items-center justify-center w-full">
+        <div className="w-full max-w-xl p-8  bg-slate-800 shadow-lg rounded-lg">
+          <FaucetForm />
+        </div>
+      </section>
     </main>
   );
 }
